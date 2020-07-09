90d550922d1ed96e9e2e482144ec25be516931cf
Luis Norman Jr <luis@Minny.local>
2020-07-09 09:20:12 -0500

    Initial upload

---
+++ mysite/blog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author', 'title', 'text')
+
+        # This formats the fields
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}), # custom class and postcontent
+            'text': forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fields = ('author', 'text')
+
+        # This formats the fields
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text': forms.TextArea(attrs={'class':'editable medium-editor-textarea'})
+        }
